4a3c791e7e6f208aa11c37a6c7c44f1dac2c17fb
Aliaksandr Fedukovich <1632637@mail.ru>
2022-04-30 00:08:38 +0300

    feat: encode-line

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,8 +11,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  
+console.log(n);
+  
+let arr = (""+n).split("").map(Number);
+console.log(arr);
+let min = Math.min(...arr);
+console.log(min)
+let i = 0;
+let answer = arr.map((item) => {
+  if (item != min) {
+    return item;
+  } else {
+    i++;
+    if(i > 1) return item;
+  }
+})
+
+return +answer.join("");
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,8 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  let sumTemp = 1;
+  let answer = [];
+
+  for (let i = 0; i < str.length - 1; i++) {
+    if(str[i] != str[i + 1] && sumTemp == 1) answer.push(str[i]);
+    if(str[i] == str[i + 1]) {
+      sumTemp++;
+    }
+    if(str[i] != str[i + 1] && sumTemp !=1) {
+      answer.push(sumTemp + str[i]);
+      sumTemp = 1;
+    }
+  }
+
+  if(sumTemp == 1) {
+    answer.push(str[str.length - 1]);
+  } else {
+    answer.push(sumTemp + str[str.length - 1]);
+  }
+
+return answer.join("");
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
