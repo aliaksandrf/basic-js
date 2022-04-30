9a67e8a911580f1384eca76176ce6b6b72c73e87
Aliaksandr Fedukovich <1632637@mail.ru>
2022-04-30 17:45:39 +0300

    feat:  add solution mine-swepper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,8 +23,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  console.log('asdf')
+  console.log(matrix);
+  let longI = matrix.length;
+  let longJ = matrix[0].length;
+
+  let answer = [];
+
+  for (let i = 0; i < longI; i++) {
+    let answerLine = [];
+    for (let j = 0; j < longJ; j++) {
+      let number = 0;
+      if (i - 1 >= 0 && j - 1 >= 0 && matrix[i-1][j-1] === true) number++;
+      if (j - 1 >= 0 && matrix[i][j-1] === true) number++;
+      if (i + 1 < longI && j - 1 >= 0 && matrix[i+1][j-1] === true) number++;
+      if (i - 1 >= 0 && matrix[i-1][j] === true) number++;
+      if (i + 1 < longI && matrix[i+1][j] === true) number++;
+      if (i - 1 >= 0 && j + 1 < longJ && matrix[i-1][j+1] === true) number++;
+      if (j + 1 < longI && matrix[i][j+1] === true) number++;
+      if (i + 1 < longI && j + 1 < longJ && matrix[i+1][j+1] === true) number++;
+      answerLine.push(number);
+      console.log(""+ i + j + number);
+    }
+    answer.push(answerLine);
+  }
+  return answer;
+
+
+
+
+
+
+
+
+/*   let long = matrix.length;
+  let answer = [];
+  for (let i = 0; i < long; i++) {
+    
+    for (let j = 0; j < long; j ++) {
+      let number = 0;
+      if(j -1 >= 0 && matrix[i][j - 1] === true) number ++;
+      if(j + 1 < long && matrix[i][j - 1] === true) number ++;
+      if(i - 1 >= 0 && j - 1 >= 0 && matrix[i-1][j-1] === true) number++;
+      if(i - 1 >= 0 && matrix[i-1][j] === true) number++;
+      if(i - 1 >= 0 && j + 1 < long && matrix[i-1][j+1] === true) number++;
+      if(i + 1 < long && j - 1 >= 0 && matrix[i+1][j-1] === true) number++;
+      if(i + 1 < long && matrix[i+1][j] === true) number++;
+      if(i + 1 < long && j + 1 < long && matrix[i+1][j+1] === true) number++;
+      answer[i][j] = number;
+
+    }
+  }
+
+  return answer; */
+  
+  
+  
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
