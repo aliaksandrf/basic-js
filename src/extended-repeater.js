94036539461a391e239ca55fae062c6c52be120b
Aliaksandr Fedukovich <1632637@mail.ru>
2022-04-30 22:47:18 +0300

    feat: add solutioin

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,8 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let str1 = String(str);
+  let repeatTimes = options.repeatTimes || 0;
+  let additionRepeatTimes = options.additionRepeatTimes || 0;
+  let separator = options.separator || "+";
+  let addition = String(options.addition);
+  let additionSeparator = options.additionSeparator || "|";
+
+  let answer = "";
+
+
+  for (let i = 0; i < repeatTimes; i++) {
+    answer += str1;
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      answer += addition;
+      if(j < additionRepeatTimes - 1) answer += additionSeparator;
+    }
+
+    if(i != repeatTimes - 1) {
+      answer += separator;
+    }
+  } 
+
+  return answer;
+
+
+
+
+
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
